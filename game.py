892745f9aaa656bf28a4052649f2d1cc5f52efb7
timfi <tim.fischer98@hotmail.com>
2017-02-05 01:49:39 +0100

    splitting up the data.xml file into different files for easier managment later on.

--- game.py
+++ game.py
@@ -1,42 +1,107 @@
 import xml.etree.ElementTree as ET
+from enum import Enum
 
 ######################################################################
 class Data():
   
-  def __init__(self, path=False):
-    if isinstance(path, str):
-      tree = ET.parse(path)
-      root = tree.getroot()
+  def __init__(self, paths={races:"races.xml", talents:"talents.xml"}):
+     self.parse_talents(self.parse_file(paths['talents']))
+     self.parse_races(self.parse_file(paths['races']))
 
-      raw_data = {}    
-      for i, child in enumerate(root):
-         raw_data[child.tag] = root[i]
+  def parse_file(self, path):
+    return self.reparse_nodes(ET.parse(path).getroot())
 
-      self.talents = []
-      for child in raw_data['talents']:
-        self.talents.append(child.text)   
+  def parse_talents(self, raw_data):
+    self.talents = {}
+    for talent in raw_data['talents']:
+      self.talents[talent.attrib['name']] = talent.text
 
-      self.races = {} 
-      for child in raw_data['races']:
-         race = {}
-         # Getting raw data
-         race_data = {}
-         for i, data in enumerate(child):
-           race_data[data.tag] = child[i]
-         # Parsing race talents
-         race['talents'] = []
-         for data in race_data['talents']:
-           if data.text in self.talents:
-             race['talents'].append(self.talents.index(data.text))
-         # Adding Race to race_dict
-         self.races[child.tag] = race
-   
+  def parse_races(self, raw_data):
+    self.races = {}
+    for race in raw_data['races']:
+       self.races[race.attrib['name']] = self.parse_race(race)
+
+  def parse_race(self, raw_data):
+    # Preparsing data for easier use
+    data = self.reparse_nodes(raw_data)
+    race = {}
+    # Parsing talents
+    race['talents'] = []
+    for talent in data['talents']:
+      if talent.text in self.talents:
+        race['talents'].append(self.talents.index(talent.text))
+
+    # return finished parse result
+    return race
+
+  def reparse_nodes(self, root):
+    raw_data = {}
+    for i, child in enumerate(root):
+      raw_data[child.tag] = root[i]
+    return raw_data
 
   def get_races(self):
     return self.races
 
   def get_talents(self):
     return self.talents
+
+######################################################################
+class Race():
+
+  def __init__(self, raw_data, talents):
+    data = {}
+    for i, child in raw_data:
+      data[child.tag] = raw_data[i]
+
+    self.talents = []
+    for talent in data['talents']:
+        if talent.text in talents:
+
+
+
+######################################################################
+class Item():
+
+  def __init__(self, raw_data):
+    data = {}
+    for i, child in enumerate(raw_data):
+      data[child.tag] = raw_data[i]
+
+    self.name = data['name']
+    self.weight = int(data['weight'])
+    self.price = int(data['price'])
+    self.special_text = data['special_text']
+    self.durability = data['durability']
+
+  def get_name(self):
+    return self.name
+
+  def get_weight(self):
+    return self.weight
+
+  def get_price(self):
+    return self.price
+
+  def get_special_text(self):
+    return self.special_text
+
+  def get_durability(self):
+    return self.durability
+
+  def sub_durability(self, val):
+    self.durability -= val
+
+class Weapon(Item):
+
+  def __init__(self, raw_data):
+    super().__init__(raw_data[0])
+    data = {}
+    for i, child in raw_data[1]:
+      data[child.tag] = raw_data[1][i]
+
+
+
 
 ######################################################################
 class Player():
@@ -71,7 +136,7 @@ class Player():
     return self.data
 
 ######################################################################
-data = Data("data.xml")
+data = Data()
 talents = data.get_talents()
 races = data.get_races()
 
