09f10bab930dd6aadbe9f86c89a63c83d6ea8ab0
CodeBlack0 <tim.fischer98@hotmail.com>
2017-02-28 18:22:06 +0100

    added some more descriptors

--- v3.0/descriptors.py
+++ v3.0/descriptors.py
@@ -26,7 +26,33 @@ class Typed(Descriptor):
         super().__set__(instance, value)
 
 
+class Integer(Typed):
+    ''' Typed-descriptor for integers '''
+    ty = int
+
+
+class Float(Typed):
+    ''' Typed-descriptor for floats '''
+    ty = float
+
+
+class List(Typed):
+    ''' Typed-descriptor for lists '''
+    ty = list
+
+
+class Dict(Typed):
+    ''' Typed-descriptor for dictionaries '''
+    ty = dict
+
+
+class String(Typed):
+    ''' Typed-descriptor for strings '''
+    ty = str
+
+
 class Positive(Descriptor):
+    ''' Descriptor for Positive Values '''
     def __set__(self, instance, value):
         if value < 0:
             raise ValueError('Must be >= 0')
@@ -34,33 +60,35 @@ class Positive(Descriptor):
 
 
 class Negative(Descriptor):
+    ''' Descriptor for Negative Values '''
     def __set__(self, instance, value):
         if value > 0:
             raise ValueError('Must be <= 0')
         super().__set__(instance, value)
 
 
-class Integer(Typed):
-    ty = int
-
-
-class Float(Typed):
-    ty = float
-
-
-class String(Typed):
-    ty = str
-
-
 class PositiveInteger(Integer, Positive):
+    ''' descriptor for postive integers'''
     pass
 
 
 class PositiveFloat(Float, Positive):
+    ''' descriptor for postive floats'''
+    pass
+
+
+class NegativeInteger(Integer, Negative):
+    ''' descriptor for negative integers'''
+    pass
+
+
+class NegativeFloat(Float, Negative):
+    ''' descriptor for negative floats'''
     pass
 
 
 class Number(Descriptor):
+    ''' descriptor for numbers (i.e. integers and floats) '''
     def __set__(self, instance, value):
         if not isinstance(value, int) and not isinstance(value, float):
             raise TypeError('Expected as number')
@@ -68,10 +96,17 @@ class Number(Descriptor):
 
 
 class PositiveNumber(Number, Positive):
+    ''' descriptor for positive numbers'''
+    pass
+
+
+class NegativeNumber(Number, Negative):
+    ''' descriptor for negative numbers'''
     pass
 
 
 class Sized(Descriptor):
+    ''' descriptor for values with a given size '''
     def __init__(self, *args, maxlen, **kwargs):
         self.maxlen = maxlen
         super().__init__(*args, **kwargs)
@@ -83,10 +118,48 @@ class Sized(Descriptor):
 
 
 class SizedString(String, Sized):
+    ''' descriptor for strings with a max size '''
+    pass
+
+
+class SizedList(List, Sized):
+    ''' descriptor for lists with a max size '''
+    pass
+
+
+class SizedDict(Dict, Sized):
+    ''' descriptor for dicts with a max size '''
+    pass
+
+
+class FixedSized(Descriptor):
+    ''' descriptor for values with a fixed size '''
+    def __init__(self, *args, size, **kwargs):
+        self.size = size
+        super().__init__(*args, **kwargs)
+
+    def __set__(self, instance, value):
+        if len(value) != self.size:
+            raise ValueError('Not the correct size')
+
+
+class FixedSizedString(String, FixedSized):
+    ''' descriptor for string with a fixed size '''
+    pass
+
+
+class FixedSizedList(List, FixedSized):
+    ''' descriptor for lists with a fixed size '''
+    pass
+
+
+class FixedSizedDict(Dict, FixedSized):
+    ''' descriptor for dicts with a fixed size '''
     pass
 
 
 class Regex(Descriptor):
+    ''' descriptor for regex-able terms '''
     def __init__(self, *args, pat, **kwargs):
         self.pat = re.compile(pat)
         super().__init__(*args, **kwargs)
@@ -98,8 +171,15 @@ class Regex(Descriptor):
 
 
 class RegexString(String, Regex):
+    ''' descriptor for string with a regex '''
     pass
 
 
 class SizedRegexString(SizedString, Regex):
+    ''' descriptor for string with a max size and a regex '''
+    pass
+
+
+class FixedSizedRegexString(FixedSizedString, Regex):
+    ''' descriptor for string with a fixed size and a regex '''
     pass
