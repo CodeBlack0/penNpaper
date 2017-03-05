5a09ee2528673982f53e88a053796d11be3738b1
CodeBlack0 <tim.fischer98@hotmail.com>
2017-03-05 03:48:57 +0100

    used the new function in tims_insanity typed lists

--- v3.0/objects.py
+++ v3.0/objects.py
@@ -1,5 +1,6 @@
 from tims_insanity.utils.classes import BaseClass
 import tims_insanity.utils.descriptors as des
+import tims_insanity.utils.types as ty
 
 
 damageTypes = {'physical': {'SLASHING', 'BLUNT', 'PIERCING', 'NONE'},
@@ -7,6 +8,20 @@ damageTypes = {'physical': {'SLASHING', 'BLUNT', 'PIERCING', 'NONE'},
 
 
 class Attack(BaseClass):
+    uuid = des.PositiveInteger()
+    name = des.String()
+    damage_type_phys = des.SetValue(set=damageTypes['physical'])
+    damage_value_phys = des.PositiveNumber()
+    damage_type_mag = des.SetValue(set=damageTypes['magical'])
+    damage_value_mag = des.PositiveNumber()
+    special_text = des.String()
+
+
+list_of_attacks = ty.generate_type_listof(ty=Attack)
+list_of_attacks_empty = list_of_attacks()
+
+
+class Resistance(BaseClass):
     uuid = des.PositiveInteger()
     name = des.String()
     damage_type_phys = des.SetValue(set=damageTypes['physical'])
@@ -24,17 +39,23 @@ class Item(BaseClass):
     special_text = des.String()
 
 
-class Weapon(Item, BaseClass):
+class Equipable(Item, BaseClass):
+    pass
+
+
+class Weapon(Equipable, BaseClass):
     uuid = des.PositiveInteger()
     name = des.String()
     price = des.PositiveNumber()
     weight = des.PositiveNumber()
     special_text = des.String()
-    attacks = des.List()
+    attacks = des.CustomTyped(ty=list_of_attacks)
     level = des.PositiveInteger()
     exp = des.PositiveNumber()
 
-atk1 = Attack(0, 'swipe', 'SLASHING', 5.0, 'NONE', 0.0, 'simple but effective sword swipe')
-wep = Weapon(1, 'short sword', 1.0, 5.0, 'simple short sword', [atk1], 1, 0.0)
 
-print(wep.__dict__)
+atk1 = Attack(0, 'swipe', 'SLASHING', 5.0, 'NONE', 0.0, 'simple but effective sword swipe')
+wep = Weapon(1, 'short sword', 1.0, 5.0, 'simple short sword', list_of_attacks_empty, 1, 0.0)
+wep.attacks.append(atk1)
+
+print(repr(wep.__dict__))
