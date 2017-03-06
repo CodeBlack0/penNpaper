2597a03192526255d986beb7c420f9e6116ca5d7
CodeBlack0 <tim.fischer98@hotmail.com>
2017-03-06 15:30:48 +0100

    added resistances and armor

--- v3.0/objects.py
+++ v3.0/objects.py
@@ -18,17 +18,19 @@ class Attack(BaseClass):
 
 
 list_of_attacks = ty.generate_type_listof(ty=Attack)
-list_of_attacks_empty = list_of_attacks()
 
 
 class Resistance(BaseClass):
     uuid = des.PositiveInteger()
     name = des.String()
-    damage_type_phys = des.SetValue(set=damageTypes['physical'])
-    damage_value_phys = des.PositiveNumber()
-    damage_type_mag = des.SetValue(set=damageTypes['magical'])
-    damage_value_mag = des.PositiveNumber()
+    resistance_type_phys = des.SetValue(set=damageTypes['physical'])
+    resistance_value_phys = des.PositiveNumber()
+    resistance_type_mag = des.SetValue(set=damageTypes['magical'])
+    resistance_value_mag = des.PositiveNumber()
     special_text = des.String()
+
+
+list_of_resistances = ty.generate_type_listof(ty=Resistance)
 
 
 class Item(BaseClass):
@@ -54,8 +56,44 @@ class Weapon(Equipable, BaseClass):
     exp = des.PositiveNumber()
 
 
-atk1 = Attack(0, 'swipe', 'SLASHING', 5.0, 'NONE', 0.0, 'simple but effective sword swipe')
-wep = Weapon(1, 'short sword', 1.0, 5.0, 'simple short sword', list_of_attacks_empty, 1, 0.0)
-wep.attacks.append(atk1)
+class Armor(Equipable, BaseClass):
+    uuid = des.PositiveInteger()
+    name = des.String()
+    price = des.PositiveNumber()
+    weight = des.PositiveNumber()
+    special_text = des.String()
+    resistances = des.CustomTyped(ty=list_of_resistances)
+    level = des.PositiveInteger()
+    exp = des.PositiveNumber()
 
-print(repr(wep.__dict__))
+
+atk1 = Attack(0, 'swipe', 'SLASHING', 5.0, 'NONE', 0.0, 'simple but effective sword swipe')
+atk2 = Attack(1, 'stab', 'PIERCING', 6.0, 'NONE', 0.0, 'simple but effective sword stab')
+wep = Weapon(1, 'short sword', 1.0, 5.0, 'simple short sword', list_of_attacks(), 1, 0.0)
+wep.attacks.append(atk1)
+wep.attacks.append(atk2)
+
+
+res1 = Resistance(0, 'heavy links', 'SLASHING', 5.0, 'NONE', 0.0, 'simple but effective against swiping')
+res2 = Resistance(1, 'tight mesh', 'PIERCING', 6.0, 'NONE', 0.0, 'simple but effective against stabbing')
+arm = Armor(1, 'chain mail', 3.0, 7.0, 'simple chainmail', list_of_resistances(), 1, 0.0)
+arm.resistances.append(res1)
+arm.resistances.append(res2)
+
+for k, v in wep.__dict__.items():
+    if not k == 'attacks':
+        print(k, ':', repr(v))
+    else:
+        print('attacks : ')
+        for a in v:
+            for k1, v1 in a.__dict__.items():
+                print('   ', k1, ':', repr(v1))
+
+for k, v in arm.__dict__.items():
+    if not k == 'resistances':
+        print(k, ':', repr(v))
+    else:
+        print('resistances : ')
+        for a in v:
+            for k1, v1 in a.__dict__.items():
+                print('   ', k1, ':', repr(v1))
